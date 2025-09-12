ed8a400ba56727ea804e464b21220f93fd2b87bd
NguyenHoangViet1501 <hoangvit2k4@gmail.com>
2025-09-12 23:50:56 +0700

    update

---
+++ src/test/java/com/webpet_nhom20/backdend/PetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webpet_nhom20.backdend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
