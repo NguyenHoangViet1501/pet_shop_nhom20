dd555a0cf15bf91ae06fd11219a1cebfe042d10a
buith <buithaibao2k4@gmail.com>
2025-10-14 23:08:35 +0700

    update get categories

--- src/main/java/com/webpet_nhom20/backdend/service/CategoryService.java
+++ src/main/java/com/webpet_nhom20/backdend/service/CategoryService.java
@@ -4,9 +4,11 @@ import com.webpet_nhom20.backdend.dto.response.CategoryResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface CategoryService {
 
-    public Page<CategoryResponse> getAllCategories(Pageable pageable);
+    public List<CategoryResponse> getAllCategories();
 
     public CategoryResponse getCategoryById(Pageable pageable ,int id);
 
