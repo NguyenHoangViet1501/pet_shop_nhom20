51617ee84c22ea4444d1a2f9f3adc67ed00fbfef
buith <buithaibao2k4@gmail.com>
2025-11-17 21:54:47 +0700

    add swagger

--- src/main/java/com/webpet_nhom20/backdend/config/OpenApiConfig.java
+++ src/main/java/com/webpet_nhom20/backdend/config/OpenApiConfig.java
@@ -1,8 +1,17 @@
 package com.webpet_nhom20.backdend.config;
 
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 
-@Configuration
-public class OpenApiConfig {
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpenApiConfig implements ApplicationListener<ApplicationReadyEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        String swaggerUrl = "http://localhost:8080/swagger-ui/index.html";
+        System.out.println("Swagger : " + swaggerUrl);
+    }
 }
